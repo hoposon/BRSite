9707cbcc206156f2100799bf4b87ba24c5ea8105
Lukas Houf <hoposon@gmail.com>
2019-03-18 18:25:24 +0100

    adding more sections to website

--- js/carousel.js
+++ js/carousel.js
@@ -4,11 +4,33 @@ let images = {
 		'../images/collection-2019-1/farhad-khodayari-1348907-unsplash.jpg',
 		'../images/collection-2019-1/journey-man-1348080-unsplash.jpg',
 		'../images/collection-2019-1/lawrson-pinson-1357054-unsplash.jpg',
-		'../images/collection-2019-1/farhad-khodayari-1348907-unsplash.jpg',
-		'../images/collection-2019-1/lawrson-pinson-1357054-unsplash.jpg',
-		'../images/collection-2019-1/lawrson-pinson-1357054-unsplash.jpg',
-		'../images/collection-2019-1/journey-man-1348080-unsplash.jpg',
-		'../images/collection-2019-1/farhad-khodayari-1348907-unsplash.jpg',
+		'../images/collection-2019-1/ali-pazani-1412360-unsplash.jpg',
+		'../images/collection-2019-1/jonathan-borba-1433863-unsplash.jpg',
+		'../images/collection-2019-1/scafeli-media-1323731-unsplash.jpg',
+		'../images/collection-2019-1/ali-pazani-1412361-unsplash.jpg',
+		'../images/collection-2019-1/motoki-tonn-1417187-unsplash.jpg',
+	],
+	coll_1: [
+		'../images/collection-2018-1/azamat-zhanisov-1339165-unsplash.jpg',
+		'../images/collection-2018-1/farhad-khodayari-1348907-unsplash.jpg',
+		'../images/collection-2018-1/journey-man-1348080-unsplash.jpg',
+		'../images/collection-2018-1/lawrson-pinson-1357054-unsplash.jpg',
+		'../images/collection-2018-1/ali-pazani-1412360-unsplash.jpg',
+		'../images/collection-2018-1/jonathan-borba-1433863-unsplash.jpg',
+		'../images/collection-2018-1/scafeli-media-1323731-unsplash.jpg',
+		'../images/collection-2018-1/ali-pazani-1412361-unsplash.jpg',
+		'../images/collection-2018-1/motoki-tonn-1417187-unsplash.jpg',
+	],
+	coll_2: [
+		'../images/collection-2017-1/azamat-zhanisov-1339165-unsplash.jpg',
+		'../images/collection-2017-1/farhad-khodayari-1348907-unsplash.jpg',
+		'../images/collection-2017-1/journey-man-1348080-unsplash.jpg',
+		'../images/collection-2017-1/lawrson-pinson-1357054-unsplash.jpg',
+		'../images/collection-2017-1/ali-pazani-1412360-unsplash.jpg',
+		'../images/collection-2017-1/jonathan-borba-1433863-unsplash.jpg',
+		'../images/collection-2017-1/scafeli-media-1323731-unsplash.jpg',
+		'../images/collection-2017-1/ali-pazani-1412361-unsplash.jpg',
+		'../images/collection-2017-1/motoki-tonn-1417187-unsplash.jpg',
 	]
 }
 
@@ -59,7 +81,6 @@ let createCarousels = function() {
 		let carInner = carousels[i].childNodes[3];
 		carInner.innerHTML = '';
 		for (let k = 0; k < picNumforPixels.picNum; k++) {
-			// li[k] = document.createElement('li', {'data-target' : '#', 'data-slide-to' : k, class: k===0 ? 'active' : ''});
 			li[k] = document.createElement('li');
 			li[k].setAttribute('data-target', '#'+carousels[i].id);
 			li[k].setAttribute('data-slide-to', k);
@@ -67,8 +88,8 @@ let createCarousels = function() {
 			carInd.appendChild(li[k]);
 		}
 
+		let picCounter = 0;
 		for (let j = 0; j < rows_num; j++) {
-			// items[j] = document.createElement('div', {class: j===0 ? 'carousel-item active' : 'carousel-item'});
 			items[j] = document.createElement('div')
 			items[j].setAttribute('class', j===0 ? 'carousel-item active' : 'carousel-item');
 			
@@ -83,9 +104,10 @@ let createCarousels = function() {
 				cols[k].setAttribute('class', 'col-'+cols_num);
 				imgs[k] = document.createElement('img');
 				imgs[k].setAttribute('class', 'd-block w-100 img-fluid');
-				imgs[k].setAttribute('src', 'http://placehold.it/250x250');
+				imgs[k].setAttribute('src', images['coll_'+i][picCounter]);
 				cols[k].appendChild(imgs[k]);
 				rows[j].appendChild(cols[k])
+				picCounter++;
 			}
 			items[j].appendChild(rows[j]);
 			carInner.appendChild(items[j]);
